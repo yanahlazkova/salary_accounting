70129c75299d4710fe50b001ab4c8d704f88c238
Programmer <Programmer@ua>
2025-12-17 09:22:34 +0200

    Розділ Довідники в меню навігації

--- salary_accounting/settings.py
+++ salary_accounting/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'persons.apps.PersonsConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static/' ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
