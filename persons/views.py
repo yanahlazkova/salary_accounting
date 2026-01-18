88dadffb843b25e6e1d8e39053118ac7d9d01686
yanahlazkova <150543917+yanahlazkova@users.noreply.github.com>
2026-01-18 14:19:10 +0200

    Update personnel

--- persons/views.py
+++ persons/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def personnel(request):
-
+    print('Hello!!!')
     return HttpResponse(f'Personnel Page, {request.user.username if request.user.is_authenticated else 'Goose'}')
