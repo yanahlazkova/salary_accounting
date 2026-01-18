2b6a6466c91b704e33988641950012dcebb96ce7
Yana <ecovod2003@ukr.net>
2026-01-18 13:13:30 +0200

    розділ Налаштування (вивід заголовків стовпців таблиці з Моделі)

---
+++ settings/models.py
@@ -0,0 +1,57 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator, DecimalValidator, MaxValueValidator
+from django.db import models
+
+
+class SocialSettings(models.Model):
+    """
+    Соціальні стандарти, що діють з певної дати.
+    Кожен новий запис позначає зміну законодавства.
+    """
+    effective_from = models.DateField(
+        verbose_name="Діє з (дата)",
+        unique=True,
+        help_text="Оберіть дату, з якої ці показники стають актуальними",
+    )
+
+    # Показники
+    min_salary = models.DecimalField(max_digits=10, decimal_places=2,
+                                     validators=[MinValueValidator(Decimal('0.01'))],  # Мінімальне значення — 1 копійка
+                                     verbose_name="Мінімальна заробітна плата (місячна)", default=1000.00)
+    pm_able_bodied = models.DecimalField(max_digits=10, decimal_places=2,
+                                         # Мінімальне значення — 1 копійка
+                                         validators=[MinValueValidator(Decimal('0.01'))],
+                                         verbose_name="Прожитковий мінімум (працездатні)", default=1000.00)
+
+    # Ставки податків
+    pdfo_rate = models.DecimalField(max_digits=5, decimal_places=2,
+                                    validators=[
+                                        MinValueValidator(Decimal('0.01'), message='Ви впевненні, що значення більше 0,00?'), # Мінімальне значення — 1 копійка
+                                        MaxValueValidator(Decimal('99.99'), message='Ви впевненні, що значення менше 100?')
+                                    ],
+                                    verbose_name="Ставка ПДФО, %", default=18.0, )
+    vz_rate = models.DecimalField(max_digits=5, decimal_places=2,
+                                  validators=[MinValueValidator(Decimal('0.01'))],  # Мінімальне значення — 1 копійка
+                                  verbose_name="Військовий збір, %", default=1.5,)
+
+    esv_rate = models.DecimalField(max_digits=5, decimal_places=2,
+                                   validators=[MinValueValidator(Decimal('0.01'))],  # Мінімальне значення — 1 копійка
+                                   verbose_name="ЄСВ, %", default=22.0)
+    # esv_invalid_rate = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="ЄСВ (інвалідність)",
+    #                                        validators=[MinValueValidator(Decimal('0.01'))],
+    #                                        # Мінімальне значення — 1 копійка
+    #                                        default=8.41)
+
+    # Дати створення/оновлення запису
+    time_created = models.DateTimeField(auto_now_add=True)  # дата при створені запису
+    time_updated = models.DateTimeField(auto_now=True)  # дата при зміні запису
+    # user_created - користувач, який створив запис
+    # user_updated - останній користувач, який змінив запис
+
+    def __str__(self):
+        return self.effective_from
+
+    class Meta:
+        db_table = 'social_settings'
+        ordering = ['-effective_from']
---
+++ settings/views.py
@@ -0,0 +1,90 @@
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import SocialSettingsForm
+from .models import SocialSettings
+
+
+def settings(request):
+    data_db = SocialSettings.objects.all()
+    table_titles = [f.verbose_name for f in SocialSettings._meta.fields]
+
+    context = {
+        'title': 'Соціальні показники',
+        'current_user': request.user.username if request.user.is_authenticated else 'Гість',
+        'buttons': [
+            {
+                'redirect_button': 'add_social_settings',
+                'icon_button': 'bi bi-gear',
+                'title_button': 'Додати',
+            },
+        ],
+        'table_titles': table_titles,
+        # 'table_titles': [
+        #     'id',
+        #     'дата початку',
+        #     'дата створення',
+        #     'дата оновлення',
+        #     'Мін.ЗП',
+        #     'Військовий збір, %',
+        #     'Прожитковий мін.',
+        #     'ПДФО, %',
+        #     'ЄСВ, %',
+        # ],
+        'table_rows': data_db
+    }
+
+    # ПЕРЕВІРКА: Чи це HTMX запит?
+    if request.headers.get('HX-Request'):
+        # Віддаємо тільки таблицю (без меню)
+        print('data_social_settings')
+        return render(request, 'data_social_settings.html', context)
+
+    # Якщо звичайний запит — віддаємо сторінку, яка "огортає" таблицю в base.html
+    print('page_social_settings')
+    return render(request, 'page_social_settings.html', context)
+
+def add_social_settings(request):
+    context = {
+        'title': 'Додати соціальні показники',
+        'current_user': request.user.username if request.user.is_authenticated else 'Гість',
+        'form_action': 'add_social_settings',
+        'buttons': [
+            {
+                'redirect_button': 'settings',
+                'icon_button': 'bi bi-backspace',
+                'title_button': 'Exit',
+            }
+        ]
+    }
+
+    if request.method == 'POST':
+        form = SocialSettingsForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            # Для HTMX краще робити редирект через заголовок HX-Redirect
+            if request.headers.get('HX-Request'):
+                response = HttpResponse()
+                response['HX-Redirect'] = reverse('settings')
+                return response
+            return redirect('settings')
+        else:
+            # Якщо форма невалідна, просто рендеримо її з помилками
+            context['form'] = form
+            return render(request, 'form_social_settings.html', context)
+    elif request.method == 'GET':
+        print(f'method = {request.method}')
+        form = SocialSettingsForm()
+        context.update({'form': form})
+        # ПЕРЕВІРКА: Чи це HTMX запит?
+        if request.headers.get('HX-Request'):
+            # Віддаємо тільки таблицю (без меню)
+            print('form_social_settings')
+            return render(request, 'form_social_settings.html', context)
+
+        # Якщо звичайний запит — віддаємо сторінку, яка "огортає" таблицю в base.html
+        print('page_form_social_settings')
+        return render(request, 'page_form_social_settings.html', context)
